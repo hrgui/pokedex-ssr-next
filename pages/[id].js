c8a3908acec6ae17a51e56b61a586351309d5d67
Harman Goei <5001432+hrgui@users.noreply.github.com>
2021-04-29 22:14:09 -0700

    meta tags

--- pages/[id].js
+++ pages/[id].js
@@ -1,23 +1,38 @@
 import Link from "next/link";
+import Head from "next/head";
 
 export default function Pokemon({ pokemon, id }) {
   return (
-    <div style={{ display: "flex", flexDirection: "column" }}>
-      <h1>{pokemon.name}</h1>
-      <img
-        style={{ margin: "0 auto" }}
-        width={300}
-        src={pokemon.sprites.other["official-artwork"]["front_default"]}
-      />
-      <div style={{ display: "flex", justifyContent: "space-between" }}>
-        <Link href={`/${+id - 1}`}>
-          <a>Prev</a>
-        </Link>
-        <Link href={`/${+id + 1}`}>
-          <a>Next</a>
-        </Link>
+    <>
+      <Head>
+        <title>#{pokemon.name}</title>
+        <meta name="twitter:card" content="summary" />
+        <meta property="og:type" content="website" />
+        <meta property="og:description" content={pokemon.name} />
+        <meta
+          property="og:image"
+          content={pokemon.sprites.other["official-artwork"]["front_default"]}
+        />
+      </Head>
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <h1>{pokemon.name}</h1>
+        <img
+          style={{ margin: "0 auto" }}
+          width={300}
+          src={pokemon.sprites.other["official-artwork"]["front_default"]}
+        />
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          {id > 1 && (
+            <Link href={`/${+id - 1}`}>
+              <a>Prev</a>
+            </Link>
+          )}
+          <Link href={`/${+id + 1}`}>
+            <a>Next</a>
+          </Link>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
